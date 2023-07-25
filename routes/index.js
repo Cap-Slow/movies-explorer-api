608063c74daaf5b0d246f9a930303e1a30f45c06
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-25 19:09:03 +0400

    feat: connect user and movie routes

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,13 @@
 const routes = require('express').Router();
+const userRoutes = require('./users');
+const movieRoutes = require('./movies');
+const NotFoundError = require('../utils/errors/notFoundError');
+const { NONEXISTENT_URL_MESSAGE } = require('../utils/constants');
+
+routes.use('', userRoutes);
+routes.use('/movies', movieRoutes);
+routes.use('*', (req, res, next) => {
+  next(new NotFoundError(NONEXISTENT_URL_MESSAGE));
+});
 
 module.exports = routes;
