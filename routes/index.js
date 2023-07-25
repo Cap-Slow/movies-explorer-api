ce770af628587b7d2e6132e0715fc100750f6445
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-25 19:22:00 +0400

    feat: import auth and protect movies routes with it

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 const routes = require('express').Router();
+const auth = require('../middlewares/auth');
 const userRoutes = require('./users');
 const movieRoutes = require('./movies');
 const NotFoundError = require('../utils/errors/notFoundError');
 const { NONEXISTENT_URL_MESSAGE } = require('../utils/constants');
 
 routes.use('', userRoutes);
-routes.use('/movies', movieRoutes);
+routes.use('/movies', auth, movieRoutes);
 routes.use('*', (req, res, next) => {
   next(new NotFoundError(NONEXISTENT_URL_MESSAGE));
 });
