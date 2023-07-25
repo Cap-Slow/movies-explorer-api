aac912e7d19a205da6e9a95958adebe9b133cc06
Dmitrii Liadov <d.liadov@xsolla.com>
2023-07-25 19:57:50 +0400

    feat: add signout route

--- routes/users.js
+++ routes/users.js
@@ -5,11 +5,13 @@ const {
   decoratedUpdateProfile,
   login,
   createUser,
+  signOut,
 } = require('../controllers/users');
 
 userRoutes.get('/users/me', auth, getUserInfo);
 userRoutes.patch('/users/me', auth, decoratedUpdateProfile);
 userRoutes.post('/signin', login);
 userRoutes.post('/signup', createUser);
+userRoutes.post('/signout', signOut);
 
 module.exports = userRoutes;
